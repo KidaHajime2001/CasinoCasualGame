1aa3be6c6ba0abcb393d0ab7394cf6171088fb87
test <test>
2023-09-19 12:36:40 +0900

    stageCreate

--- Assets/MySctipt/Test/GameDataManager.cs
+++ Assets/MySctipt/Test/GameDataManager.cs
@@ -2,6 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.Rendering.LookDev;
+using UnityEngine.UI;
+using TMPro;
+using UnityEditor;
 
 public class GameDataManager : MonoBehaviour
 {
@@ -76,20 +80,53 @@ public class GameDataManager : MonoBehaviour
     [SerializeField]
     private  List<GameWave> waves;
     private List<GameWave> _data;
+    [SerializeField]
+    private GameData Data;
+    private GameData gameData;
+
+    [SerializeField]GameObject inputFieldObj;
+    TMP_InputField inputField;
+    [SerializeField]
+    private GameObject outputFieldObj;
+
     // ファイルパス
     private string _dataPath;
-
+    private string _InputPath;
     private void Awake()
     {
+        inputField = inputFieldObj.GetComponent<TMP_InputField>();
+        Debug.Log(inputField);
         _dataPath = Application.persistentDataPath + "/GameWaveData1.json";
         Debug.Log(Application.persistentDataPath);
-        _data = LoadData(_dataPath);
-        SaveData(waves);
-        //Debug.Log(waves[0]);
+
+        gameData = LoadGameData(_dataPath);
+        
+        SaveGameData(Data);
+        Debug.Log(Data);
+    }
+    public void InputText()
+    {
+      
+    }
+    public void CreateGameData()
+    {
+        Debug.Log(inputField.text);
+        _InputPath = Application.dataPath + "/JsonFile/"+inputField.text+".json";
+        Debug.Log("CreateData:" + _InputPath);
+        gameData=LoadGameData(_InputPath);
+        SaveGameData(gameData);
+
     }
 
-
     private void SaveData(List<GameWave> data)
+    {
+        string jsonstr = JsonUtility.ToJson(data);//受け取ったPlayerDataをJSONに変換
+        StreamWriter writer = new StreamWriter(_dataPath, false);//初めに指定したデータの保存先を開く
+        writer.WriteLine(jsonstr);//JSONデータを書き込み
+        writer.Flush();//バッファをクリアする
+        writer.Close();//ファイルをクローズする
+    }
+    private void SaveGameData(GameData data)
     {
         string jsonstr = JsonUtility.ToJson(data);//受け取ったPlayerDataをJSONに変換
         StreamWriter writer = new StreamWriter(_dataPath, false);//初めに指定したデータの保存先を開く
@@ -104,7 +141,7 @@ public class GameDataManager : MonoBehaviour
         {
             Debug.Log("ファイルが見当たりませんでした");
             List<GameWave> data = new List<GameWave>();
-            
+
             data = waves;
             string jsonstr = JsonUtility.ToJson(data);//受け取ったPlayerDataをJSONに変換
             StreamWriter writer = new StreamWriter(dataPath, false);//初めに指定したデータの保存先を開く
@@ -113,13 +150,30 @@ public class GameDataManager : MonoBehaviour
             writer.Close();//ファイルをクローズする
             Debug.Log(data);
         }
-
-        
-
         // JSONデータとしてデータを読み込む
         var json = File.ReadAllText(dataPath);
 
         return JsonUtility.FromJson<List<GameWave>>(json);
     }
+    private GameData LoadGameData(string dataPath)
+    {
+        if (!File.Exists(dataPath))
+        {
+            Debug.Log("ファイルが見当たりませんでした");
+            GameData data = new GameData();
+            data = Data;
+            string jsonstr = JsonUtility.ToJson(data);//受け取ったPlayerDataをJSONに変換
+            StreamWriter writer = new StreamWriter(dataPath, false);//初めに指定したデータの保存先を開く
+            writer.WriteLine(jsonstr);//JSONデータを書き込み
+            writer.Flush();//バッファをクリアする
+            writer.Close();//ファイルをクローズする
+            Debug.Log(data);
+        }
+        // JSONデータとしてデータを読み込む
+        var json = File.ReadAllText(dataPath);
 
+        //return JsonUtility.FromJson<List<GameWave>>(json);
+        return JsonUtility.FromJson<GameData>(json);
+    }
 }
+
